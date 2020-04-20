0506c165f2615960c8d8082d13f43ddbca70e9bc
Fernando Martin <fernancoder@gmail.com>
2020-04-20 12:19:01 +0200

    Clase user model

--- UserModel.cpp
+++ UserModel.cpp
@@ -1,8 +1,23 @@
 #include "UserModel.h"
 
+
+
+UserModel::UserModel() {
+  pthread_mutex_init(&user_model_lock,NULL);
+
+}
+
+UserModel::~UserModel() {
+  pthread_mutex_destroy(&user_model_lock);
+}
+
+
 void UserModel::add_user(char* user_id, char *token)
 {
+   pthread_mutex_lock(&user_model_lock);
 
+
+   pthread_mutex_unlock(&user_model_lock);
 }
 
 
