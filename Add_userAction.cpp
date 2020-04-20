05c41dbde4040c6d800c66e30e4084844a483098
Fernando Martin <fernancoder@gmail.com>
2020-04-20 20:06:33 +0200

    refs #13543 exportar ejecuciones finalizadas en excel

--- Add_userAction.cpp
+++ Add_userAction.cpp
@@ -11,14 +11,16 @@ void Add_userAction::execute()
 
     if ( !verifyParam(this->getRequestParam("user_uuid"),1000,"Parameter user_uuid is mandatory") )
     {
-	this->closeConnection();
-	return;
+	  this->closeConnection();
+	  return;
     }
     if ( !verifyParam(this->getRequestParam("token"),1000,"Parameter token is mandatory") )
     {
-	this->closeConnection();
-	return;
+	  this->closeConnection();
+	  return;
     }
+
+    this->getServer()->userModel->add_user(this->getRequestParam("user_uuid"), this->getRequestParam("token"))
 
 
     //Change this and put your stuff here
