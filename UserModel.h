19aa5818a91afcfbc72a7f3a9663ebf330bbb77d
Fernando Martin <fernancoder@gmail.com>
2020-04-20 13:14:55 +0200

    Clase user model

--- UserModel.h
+++ UserModel.h
@@ -7,11 +7,12 @@
 class UserModel
 {
     public:
-      UserModel();
+      UserModel(string user_file_path);
       ~UserModel();
       void add_user(string user_id, string token);
     private:
       pthread_mutex_t user_model_lock;
+      string user_file_path;
 
 };
 
