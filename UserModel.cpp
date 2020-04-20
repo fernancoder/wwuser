42e37d718269c7fc37d30489a2f8d5a2416a0fef
Fernando Martin <fernancoder@gmail.com>
2020-04-20 13:13:29 +0200

    Clase user model

--- UserModel.cpp
+++ UserModel.cpp
@@ -1,8 +1,15 @@
 #include "UserModel.h"
 
-UserModel::UserModel()
+UserModel::UserModel(string user_file_path)
 {
+  this->user_file_path = user_file_path;
   pthread_mutex_init(&user_model_lock,NULL);
+
+  //read file
+  pthread_mutex_lock(&user_model_lock);
+
+
+  pthread_mutex_unlock(&user_model_lock);
 }
 
 UserModel::~UserModel()
@@ -13,10 +20,10 @@ UserModel::~UserModel()
 
 void UserModel::add_user(string user_id, string token)
 {
-   pthread_mutex_lock(&user_model_lock);
+  pthread_mutex_lock(&user_model_lock);
 
 
-   pthread_mutex_unlock(&user_model_lock);
+  pthread_mutex_unlock(&user_model_lock);
 }
 
 
