4d3d13b84a5771ab40c9786608a02645ab884e07
Fernando Martin <fernancoder@gmail.com>
2020-05-15 13:04:34 +0200

    Notificar cambios

--- UserModel.cpp
+++ UserModel.cpp
@@ -241,7 +241,7 @@ void UserModel::send_notification(char *user_id, char *entry_title)
         strcat(payload, "\"}");
 
         string url = "POST https://fcm.googleapis.com/fcm/send HTTP/1.1\r\nContent-Type: application/json\r\nContent-Length: " + Util::intToString((int)strlen(payload)) + "\r\nAuthorization: key=AAAAdh166Bg:APA91bFLhhUmyIIrWakVXjZRyER3uOFgc_r6pJMvzTxWV7kW64aM3VovXGlrA1IKw2rdjxrNwyLP2IR64TLj9HuyOn5-Juj_YYzA7P1KqupAknfOFP8p28PjFezJNgFimmQYjEwNPoxz\r\nConnection: close\r\n\r\n";
-        //printf("ENVIO: %s\n%s", url.c_str(), payload);
+        printf("ENVIO: %s\n%s", url.c_str(), payload);
         if ( pushNotification->get((char *)(url.c_str()), payload) )
         {
 
