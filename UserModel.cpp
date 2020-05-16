3437b05dc2498b01e3449b2045e5088a8c5c9978
Fernando Martin <fernancoder@gmail.com>
2020-05-16 12:23:49 +0200

    Notificar cambios

--- UserModel.cpp
+++ UserModel.cpp
@@ -203,14 +203,15 @@ string UserModel::notify_changes()
             {
               memcpy(update_date, response_date, 20);
               update_date[20] = '\0';
-              printf("[%s]%s -> %s (%s)\n", (*it)->user_id, (*it)->entry_title, update_date, (*it)->last_update);
+              printf("[%s]%s -> %s (%s)", (*it)->user_id, (*it)->entry_title, update_date, (*it)->last_update);
 
               if ( strcmp(update_date,(*it)->last_update) > 0 )
               {
                 //printf("Send change notification for %s to %s\n", (*it)->entry_title, (*it)->user_id);
-                //printf("Envío\n");
+                printf(" ENVIADO");
                 send_notification((*it)->user_id, update_date, (*it)->entry_title);
               }
+              printf("\n");
             }
           }
         }
